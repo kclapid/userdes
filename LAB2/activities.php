b5daadcf78f6b0990c8adb432bc3a732f176c15f
kclapid <52141585+kclapid@users.noreply.github.com>
2019-08-19 21:34:42 +0800

    Update and rename activities.html to activities.php

---
+++ LAB2/activities.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>K•Y•L•A | Activities</title>
+	<link rel="icon" type="image/ico" href="img/black-rose1.jpg">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body class="text-font">
+
+<header>
+<nav class="navbar navbar-expand-sm bg-light navbar-light fixed-top" id="nav1">
+<div class="container-fluid">
+	<a class="navbar-brand" href="index.php" class="btn btn-outline-secondary"><img class="logo" src="img/sunflower.png"></a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarResponsive">
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item active">
+				<a href="index.php" class="btn btn-outline-dark">Home</a>
+			</li>
+			<li class="nav-item">
+				<a href="LAB1\index.php" class="btn btn-outline-dark">About</a>
+			</li>
+			<li class="nav-item">
+				<a href="reflections.php" class="btn btn-outline-dark">Reflections</a>
+			</li>
+			<li class="nav-item">
+				<a href="quiz.php" class="btn btn-outline-dark">Quiz</a>
+			</li>
+		</ul>
+
+		
+	
+</div>
+</nav>
+</header>
+
+<ul>
+<li><a class="btn-act1" href="activity1.php">Activity 1</a></li>
+<hr>
+<li><a class="btn-act1" href="activity2.php">Activity 2</a></li>
+<hr>
+<li><a class="btn-act1" href="activity3.php">Activity 3</a></li>
+<hr>
+<li><a class="btn-act1" href="activity4.php">Activity 4</a></li>
+<hr>
+<li><a class="btn-act1" href="activity5.php">Activity 5</a></li>
+<hr>
+<li><a class="btn-act1" href="activity6.php">Activity 6</a></li>
+<hr>
+<li><a class="btn-act1" href="activity7.php">Activity 7</a></li>
+</ul>
+
+
+</body>
+</html>
