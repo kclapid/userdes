6351991b65f4202f4172cd017f8305285cae8476
kclapid <52141585+kclapid@users.noreply.github.com>
2019-09-05 07:57:07 +0800

    Update and rename activity8.html to activity8.php

---
+++ LAB2/activity8.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>K•Y•L•A | Activity 8</title>
+	<link rel="icon" type="image/ico" href="img/black-rose1.jpg">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header>
+<nav class="navbar navbar-expand-sm bg-light navbar-light fixed-top" id="nav1">
+<div class="container-fluid">
+
+<a href="activities.php"><h3 class="back"><</h3></a>
+	
+	<a class="navbar-brand" href="index.php"><img class="logo" src="img/sunflower.png"></a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarResponsive">
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item active">
+				<a href="index.php" class="btn btn-outline-dark">Home</a>
+			</li>
+			<li class="nav-item">
+				<a href="LAB1\index.php" class="btn btn-outline-dark">About</a>
+			</li>
+			<li class="nav-item">
+				<a href="reflections.php" class="btn btn-outline-dark">Reflections</a>
+			</li>
+			<li class="nav-item">
+				<a href="quiz.php" class="btn btn-outline-dark">Quiz</a>
+			</li>
+		</ul>
+	
+</div>
+</nav>
+</header>
+
+<div class="container-fluid padding">	
+<div class="row welcome text-center">
+	<div class="col-12">
+		<h1 class="display-4">Grid Layout Design</h1>
+	</div>
+	<hr>
+</div>
+</div>	
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<!-- Trigger the Modal -->
+<!-- 4x4 layout grid -->
+<p><center>4x4 Layout</center></p>
+<p>
+<center>
+<img class="myImg" src="img/4a1.PNG" alt="4x4 layout 1" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/4a2.PNG" alt="4x4 layout 2" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/4a3.PNG" alt="4x4 layout 3" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/4a4.PNG" alt="4x4 layout 4" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/4a5.PNG" alt="4x4 layout 5" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/4a6.PNG" alt="4x4 layout 6" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/4a7.PNG" alt="4x4 layout 7" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/4a8.PNG" alt="4x4 layout 8" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/4a9.PNG" alt="4x4 layout 9" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/4a10.PNG" alt="4x4 layout 10" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/4a11.PNG" alt="4x4 layout 11" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/4a12.PNG" alt="4x4 layout 12" style="width:100%;max-width:300px;padding-bottom: 10px">
+</center>
+</p>
+<!-- 4x4 pictures -->
+<p><center>4x4 pictures</center></p>
+<p>
+<center>
+<img class="myImg" src="img/4b1.PNG" alt="4x4 picture 1" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/4b2.PNG" alt="4x4 picture 2" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/4b3.PNG" alt="4x4 picture 3" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/4b4.PNG" alt="4x4 picture 4" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/4b5.PNG" alt="4x4 picture 5" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/4b6.PNG" alt="4x4 picture 6" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/4b7.PNG" alt="4x4 picture 7" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/4b8.PNG" alt="4x4 picture 8" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/4b9.PNG" alt="4x4 picture 9" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/4b10.PNG" alt="4x4 picture 10" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/4b11.PNG" alt="4x4 picture 11" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/4b12.PNG" alt="4x4 picture 12" style="width:100%;max-width:300px;padding-bottom: 10px">
+</center>
+</p>
+
+<p><center>3x3 Layout</center></p>
+<p>
+<!-- Trigger the Modal -->
+<!-- 3x3 layout grid -->
+<center>
+<img class="myImg" src="img/3a1.PNG" alt="3x3 layout 1" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/3a2.PNG" alt="3x3 layout 2" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/3a3.PNG" alt="3x3 layout 3" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/3a4.PNG" alt="3x3 layout 4" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/3a5.PNG" alt="3x3 layout 5" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/3a6.PNG" alt="3x3 layout 6" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/3a7.PNG" alt="3x3 layout 7" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/3a8.PNG" alt="3x3 layout 8" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/3a9.PNG" alt="3x3 layout 9" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/3a10.PNG" alt="3x3 layout 10" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/3a11.PNG" alt="3x3 layout 11" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/3a12.PNG" alt="3x3 layout 12" style="width:100%;max-width:300px;padding-bottom: 10px">
+</center>
+</p>
+<!-- 3x3 pictures -->
+<p><center>3x3 pictures</center></p>
+<p>
+<center>
+<img class="myImg" src="img/3b1.PNG" alt="3x3 picture 1" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/3b2.PNG" alt="3x3 picture 2" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/3b3.PNG" alt="3x3 picture 3" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/3b4.PNG" alt="3x3 picture 4" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/3b5.PNG" alt="3x3 picture 5" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/3b6.PNG" alt="3x3 picture 6" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/3b7.PNG" alt="3x3 picture 7" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/3b8.PNG" alt="3x3 picture 8" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/3b9.PNG" alt="3x3 picture 9" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/3b10.PNG" alt="3x3 picture 10" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/3b11.PNG" alt="3x3 picture 11" style="width:100%;max-width:300px;padding-bottom: 10px">
+<img class="myImg" src="img/3b12.PNG" alt="3x3 picture 12" style="width:100%;max-width:300px;padding-bottom: 10px">
+</center>
+</p>
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- The Close Button -->
+  <span class="close" onclick="document.getElementById('myModal').style.display='none'">&times;</span>
+
+  <!-- Modal Content (The Image) -->
+  <img class="modal-content" id="img01">
+
+  <!-- Modal Caption (Image Text) -->
+  <div id="caption"></div>
+</div>
+</p>
+<script type="text/javascript">
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = $('.myImg');
+var modalImg = $("#img01");
+var captionText = document.getElementById("caption");
+$('.myImg').click(function(){
+    modal.style.display = "block";
+    var newSrc = this.src;
+    modalImg.attr('src', newSrc);
+    captionText.innerHTML = this.alt;
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+</script>
+	
+</div>
+</body>
+</html>
