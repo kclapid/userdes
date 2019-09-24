3a2cd49452305c71ef97ff8b6158b5155182e337
kclapid <52141585+kclapid@users.noreply.github.com>
2019-09-24 20:44:05 +0800

    Update activities.php

--- LAB2/activities.php
+++ LAB2/activities.php
@@ -36,7 +36,7 @@
 				<a href="activities.php" class="btn btn-outline-dark">Activities</a>
 			</li>
 			<li class="nav-item">
-				<a href="quiz.php" class="btn btn-outline-dark">Quiz</a>
+				<a href="learnings.html" class="btn btn-outline-dark" class="btn btn-outline-dark">Learnings</a>
 			</li>
 		</ul>
 
